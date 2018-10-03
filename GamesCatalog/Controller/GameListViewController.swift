b237ebf301627193d0249bcd3d76b460d00bab0a
Diego Lopes <diegolopes0501@gmail.com>
2018-10-02 22:32:17 -0300

    initial struct with GameList tableView

---
+++ GamesCatalog/Controller/GameListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  GamesCatalog
+//
+//  Created by Diego Lopes on 02/10/18.
+//  Copyright © 2018 Diego Lopes. All rights reserved.
+//
+
+import UIKit
+
+class GameListViewController: UITableViewController {
+
+    let itemArray = ["Game 1", "Game 2", "Game 3", "Game 4", "Game 5"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GameListCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
